00743d6724bbb3e174908cd6542d05e7af39e10c
Jefairfi@gmail.com <Arcti311>
2016-09-29 21:59:36 -0700

    This is a test commit

--- LibertyWildlifeApp/LibertyWildlifeApp.iOS/Main.cs
+++ LibertyWildlifeApp/LibertyWildlifeApp.iOS/Main.cs
@@ -14,6 +14,7 @@ namespace LibertyWildlifeApp.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            //Test Comment
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
